6aea488a9c74d93f426b5c3ba5e69f794a014989
= <=>
2022-10-03 11:53:17 -0300

    Adicionado condição para que seja possível localizar qualquer chave no arquivo env chamado MANAGER, onde caso houver qualquer parametro iniciado com este nome, será localizado e atribuido as managers da aplicação

--- infectado/services/configs.py
+++ infectado/services/configs.py
@@ -7,6 +7,7 @@ data_env: dict = Env.get_values()
 __URL_SOCKET__: str = data_env['URL_CONNECTION_SERVER']
 
 __MANAGERS__: list[str] = [
-    data_env['MANAGER_AUTOMATE_BROWSER'],
-    data_env['MANAGER_AUTOMATE_SO']
+    value_env
+    for key_env, value_env in data_env.items()
+    if (key_env or '' ).upper().startswith('MANAGER')
 ]
